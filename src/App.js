39933604ef00598702eecea6f263be85062586de
castynet <riungemaina@gmail.com>
2021-01-26 22:42:59 +0300

    App: imported required base modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
+import { useApi } from "./components/context";
+
 export default function App() {
   return (
     <>
+      {/* <Router> */}
       <p>Hello Love</p>
+      {/* </Router> */}
     </>
   );
 }
